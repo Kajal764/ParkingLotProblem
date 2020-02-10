2d32cc26653e1e1753bfd0bc59443354f299d1a6
kajal764 <kajaldw666@gmail.com>
2020-02-10 17:37:54 +0530

    Added:Function To Unpark The Car And Assign Its Space Empty

--- src/main/java/parkinglot/ParkingLot.java
+++ src/main/java/parkinglot/ParkingLot.java
@@ -1,20 +1,41 @@
 package parkinglot;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class ParkingLot {
-    int token=0;
+
+    TreeMap<Integer, CarInfo> data = new TreeMap<Integer, CarInfo>();
+    TreeMap<Integer, Boolean> isEmpty = new TreeMap<Integer, Boolean>();
+    Integer token = 0;
+
     public boolean park() {
         return true;
     }
 
-    public HashMap<Integer, Boolean> addedInfo(CarInfo info) {
-        HashMap<Integer, CarInfo> data=new HashMap<>();
-        HashMap<Integer,Boolean> isEmpty=new HashMap<Integer,Boolean>();
-        token++;
-        CarInfo put = data.put(token,info);
-        isEmpty.put(token,true);
-       return isEmpty;
+    public TreeMap<Integer, Boolean> addedInfo(CarInfo info) {
+        boolean park = park();
+        if (park == true) {
+            token++;
+            CarInfo put = data.put(token, info);
+            isEmpty.put(token, true);
+        }
+        return isEmpty;
     }
 
-}
+    public void unPark(int key) {
+       isEmpty.replace(key, false);
+    }
+    }
+
+
+//        Boolean replace = isEmpty.replace(integerBooleanHashMap,false);
+//        System.out.println(isEmpty);
+//        return replace;
+
+
+
+//    public void unPark(HashMap<Integer, Boolean> integerBooleanHashMap) {
+//        isEmpty.replace(integerBooleanHashMap.,false);
+//    }
--- src/test/java/parkinglot/ParkingLotTestCases.java
+++ src/test/java/parkinglot/ParkingLotTestCases.java
@@ -1,8 +1,11 @@
 package parkinglot;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class ParkingLotTestCases {
 
@@ -21,11 +24,13 @@ public class ParkingLotTestCases {
         CarInfo car3=new CarInfo("TN 7221","Mpv","white",2.15);
         CarInfo car4=new CarInfo("RMD 387","Mrecedes","blue",2.30);
         CarInfo car5=new CarInfo("AD 221","AudiR8","black",2.45);
-        HashMap<Integer, Boolean> integerBooleanHashMap1 = parkingLot.addedInfo(car1);
-        HashMap<Integer, Boolean> integerBooleanHashMap2 = parkingLot.addedInfo(car2);
-        HashMap<Integer, Boolean> integerBooleanHashMap3 = parkingLot.addedInfo(car3);
-        HashMap<Integer, Boolean> integerBooleanHashMap4 = parkingLot.addedInfo(car4);
-        HashMap<Integer, Boolean> integerBooleanHashMap5 = parkingLot.addedInfo(car5);
+
+        TreeMap<Integer, Boolean> integerBooleanHashMap1 = parkingLot.addedInfo(car1);
+        TreeMap<Integer, Boolean> integerBooleanHashMap2 = parkingLot.addedInfo(car2);
+        TreeMap<Integer, Boolean> integerBooleanHashMap3 = parkingLot.addedInfo(car3);
+        TreeMap<Integer, Boolean> integerBooleanHashMap4 = parkingLot.addedInfo(car4);
+        TreeMap<Integer, Boolean> integerBooleanHashMap5 = parkingLot.addedInfo(car5);
+
         Assert.assertEquals(integerBooleanHashMap1.get(1),true);
         Assert.assertEquals(integerBooleanHashMap2.get(2),true);
         Assert.assertEquals(integerBooleanHashMap3.get(3),true);
@@ -34,4 +39,24 @@ public class ParkingLotTestCases {
     }
 
 
+    @Test
+    public void whenParkCar_ItShouldFirstCheckSpaceIsEmpty_ThenSpaceTokenShouldReturnTrue() {
+        ParkingLot parkingLot= new ParkingLot();
+        CarInfo car1=new CarInfo("KL 7221","SuzukiSwift","white",2.0);
+        TreeMap<Integer, Boolean> integerBooleanHashMap = parkingLot.addedInfo(car1);
+        Assert.assertEquals(integerBooleanHashMap.get(1),true);
+    }
+
+    @Test
+    public void whenUnparkThecar_ItShouldAssignSpaceEmptyForThatToken() {
+        ParkingLot parkingLot=new ParkingLot();
+        CarInfo car1=new CarInfo("KL 7221","SuzukiSwift","white",2.0);
+        CarInfo car2=new CarInfo("MH 01 CD 3367","Suv","red",2.15);
+        TreeMap<Integer, Boolean> integerBooleanHashMap1 = parkingLot.addedInfo(car1);
+        TreeMap<Integer, Boolean> integerBooleanHashMap2 = parkingLot.addedInfo(car2);
+        int token=2;
+        parkingLot.unPark(token);
+        System.out.println(integerBooleanHashMap2);
+    }
 }
+
